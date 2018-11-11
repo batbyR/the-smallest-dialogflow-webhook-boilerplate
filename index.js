3ce6227d1e2c7eae9c13b0fbcad57c0b267feec3
batbyR <batby@ensta.fr>
2018-11-11 12:11:58 +0100

    Return json object

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-'use strict'
+"use strict";
 
-let express = require('express')
-let app = express()
-let bodyParser = require('body-parser')
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
 
 // These two following lines ensures that every incomming request is parsed to json automatically
-app.use(bodyParser.urlencoded({ extended: 'true' }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: "true" }));
+app.use(bodyParser.json());
 // Allowing access to resources from anywhere
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   let response = {};
   const intentName = req.body.queryResult.intent.displayName;
 
-  if (intentName === 'hello') {
+  if (intentName === "hello") {
     response = {
-      fulfillmentText: "Hello",
-    }
+      fulfillmentText: "Hello"
+    };
   }
 
   res.json(response);
-})
-
-app.get('/health', (req, res) => {
-  res.send('ok prod')
 });
 
-app.post('/health', (req, res) => {
-  res.send('ok prod')
+app.get("/health", (req, res) => {
+  res.send("ok prod");
+});
+
+app.post("/health", (req, res) => {
+  const response = { fulfillmentText: "OK GO" };
+  res.send(JSON.stringify(response));
 });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port)
-console.log('info', `server listening on port ${port}`)
+app.listen(port);
+console.log("info", `server listening on port ${port}`);
